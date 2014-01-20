b526dc1114977e55e336f7351ec2e22a78793375
Erik Feucht <ErikFeucht@gmail.com>
2014-01-20 18:14:34 +0100

    stubbing out testing files

--- app/test/gdp_test.dart
+++ app/test/gdp_test.dart
@@ -3,6 +3,27 @@ library gdp_test;
 import '../main.dart';
 import 'package:unittest/unittest.dart';
 
+part 'animations/sprite_sheet_test.dart';
+part 'audio/sound_manager_test.dart';
+
+part 'camera/camera_test.dart';
+part 'camera/adjusted_context_test.dart';
+
+part 'levels/level_manager_test.dart';
+
+part 'objects/game_object_test.dart';
+part 'objects/alien_test.dart';
+part 'objects/block_test.dart';
+part 'objects/enemy_test.dart';
+part 'objects/gorilla_test.dart';
+part 'objects/item_test.dart';
+part 'objects/oxygen_test.dart';
+part 'objects/player_test.dart';
+part 'objects/ship_item_test.dart';
+part 'objects/spike_test.dart';
+
+part 'input_manager_test.dart';
+part 'object_manager_test.dart';
 
 
 main() {
