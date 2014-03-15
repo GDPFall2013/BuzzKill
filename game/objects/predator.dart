7044039c6255b67f11ff93438e0aaa2c5ad4534b
DANIEL OSOBU <dosobu007@gmail.com>
2014-03-15 09:19:28 -0500

    Added Predator Object Animation drawings and code

---
+++ game/objects/predator.dart
@@ -0,0 +1,176 @@
+part of gdp;
+
+class Predator extends Enemy
+{
+  
+  ImageElement img = new ImageElement();
+
+  SpriteSheet sprite;
+  SpriteSheet attackSprite;
+
+  
+  int spriteXInitial = 0;
+  int spriteYInitial = 1825;
+  int spriteWidth = 180;
+  int spriteHeight = 220;
+  //int attacknum;
+  
+ 
+  
+  double imgOffsetX = 0.0;
+  double imgOffsetY = -20.0;
+
+  double initialPos;
+  double endPos;
+  bool goingBack = false;
+  
+  
+  bool triggerFall = false;
+  bool attack = false;
+  
+  //Random rnd = new Random();
+ 
+  /* generateRandomNumbers()
+  {
+    
+    attacknum = rnd.nextInt(2);
+    
+  }*/
+  
+  initialize(double x, double y) {
+    super.initialize(x, y);
+    width = 180.0;
+    height = 220.0;
+    
+    sprite = new SpriteSheet("./content/enemies_spritesheet.png",0,1825,180,220);
+    attackSprite = new SpriteSheet("./content/Predator_attack_spritesheet.png",0,305,200,240);
+    
+    initialPos = x;
+    endPos = x-150.0;
+    
+    sprite.frameChangeRate = 50.0;
+    sprite.numberOfFrames = 6;
+    
+    attackSprite.frameChangeRate = 300.0;
+    attackSprite.numberOfFrames = 4;
+    
+    
+       
+     TYPE = "BOSS";
+     
+     
+  }
+  
+  update (double dt) 
+  {
+    double speed = 0.4 * dt;
+    
+    if(triggerFall)
+    {
+      y += 1;
+      sprite.spritey = 130;
+      
+      /*
+      if (sprite.lastDraw > sprite.frameChangeRate) {
+          sprite.lastDraw -= sprite.frameChangeRate;
+            if (sprite.spriteFrame >= sprite.numberOfFrames) {  
+              sprite.spriteFrame = 1;
+            }
+            else{
+              sprite.spriteFrame++;
+            }
+            // The minus one is because the first frame starts at 0
+            if(sprite.spritex!=378 && sprite.spritex!=504 && sprite.spritex!=630){
+                 sprite.spritex = 378;}
+                 else{
+                   if(sprite.spritex == 630){
+                     sprite.spritex = 378;
+                   }
+                   else{
+                     sprite.spritex += 126;
+                   }
+                 }
+          }
+      */
+    }
+    
+    
+    //move Predator back and forth
+    else{
+    if (goingBack == false) {
+      if(x >= endPos){
+        x = x - speed;
+      }
+      else{
+        goingBack = true;
+        sprite.spritey = 2073;
+      }
+    }
+    else {
+      if(x<initialPos){
+        x = x + speed;
+      }
+      else{
+        goingBack = false;
+        sprite.spritey = 1825;
+      }
+    }
+    }
+    
+    sprite.update(dt);  
+    
+    if(attack)
+    {
+      /* if(attackSprite.spritex >= 400){
+         attackSprite.spritex =0;}
+       else{
+         attackSprite.spritex += 200;
+       }*/
+      
+    
+       if(attackSprite.spriteFrame > 3)
+       {
+        attack = false;
+        attackSprite.spritex = 0;
+        sprite.spritex = 0;
+        attackSprite.spriteFrame = 1;
+       }
+       else if(goingBack)
+       {
+         attackSprite.spritey = 22;
+         sprite.spritex = -200;
+       }
+       else
+       {
+        attackSprite.spritey = 305;
+        sprite.spritex = -200;
+       } 
+       
+       attackSprite.update(dt);
+     }
+     
+       
+       attackSprite.update(dt);
+    }
+
+   
+  
+  
+  draw()
+  {
+    sprite.drawOnPosition(x-width/2 - imgOffsetX, y-height/2 + imgOffsetY, width , height);
+    if(attack)
+    {
+       attackSprite.drawOnPosition(x-width/2 - imgOffsetX, y-height/2 + (-10), width , height);
+       
+    }
+  }
+  
+   double injure() 
+   {
+    attack = true;
+    //generateRandomNumbers();
+    return 10.0;
+   }  
+   
+}  
